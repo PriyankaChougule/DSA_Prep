cfc771fd088b3897cc6a9b26f9624c35f5ff819a
Priyanka Chougule <priyankachgldkte@gmail.com>
2025-07-17 06:51:01 +0530

    Initial commit

---
+++ src/ReverseArrayInPlace.java
@@ -0,0 +1,21 @@
+public class ReverseArrayInPlace {
+
+    public static void main(String[] args) {
+        int[] a = {10,20,3,4,80,1};
+        reverseArray(a);
+    }
+
+    static void reverseArray(int[] a) {
+        int temp;
+        int size = a.length;
+
+        for(int i=0; i<size/2; i++) {
+            temp  = a[i];
+            a[i] = a[size-i-1];
+            a[size-i-1] = temp;
+        }
+
+        for(int i : a)
+            System.out.println(i);
+    }
+}
